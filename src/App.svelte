598cff01258a81873d94fc167a872402e380a9d7
HARSH KUMAR <harsh@HARSHs-MacBook-Air.local>
2021-09-30 19:07:12 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	import Player from './Player.svelte';
+	import Addplayer from './Addplayer.svelte'
+	let players = [
+		{
+			name: "Jhon Doe",
+			points: 53
+		},
+		{
+			name: "Sam Smith",
+			points: 45
+		},	
+		{
+			name: "Sara Wilson",
+			points: 34,
+		}
+	];
+	const addplayer = (e) => {
+		const newPlayer = e.detail;
+		players = [...players, newPlayer];
+	
+	};
+	const removePlayer = e => {
+		players = players.filter(player => player.name !== e.detail);
+	};
+</script>
+
+<main>
+	<Navbar />
+	<div class="container">
+		<Addplayer on:addplayer = {addplayer}/>
+		{#if players.length === 0}
+			<p>No Player to show</p>
+		{:else}
+			{#each players as player}
+				<Player name = {player.name} points = {player.points}
+				on:removeplayer = {removePlayer	} />
+			{/each}
+		{/if}
+	</div>
+</main>
---
+++ src/Player.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let name;
+    export let points;
+	let showControls = true;
+
+	const addPoint = () => points++;
+	const removePoint = () => points--;
+	const toggleControls = () => (showControls = !showControls);
+    const onDelete = () => dispatch("removeplayer", name);  
+</script>
+
+<main>
+    <div class="card">
+        <h1> {name}
+        <button class="btn btn-sm" on:click= {toggleControls}>
+        {#if showControls} - {:else} +	
+        {/if}</button>
+        <button class="btn btn-danger btn-sm" on:click={onDelete}>x</button>
+        </h1>
+        <h3>Points:{points}</h3>
+        {#if showControls}
+        <button class="btn" on:click={addPoint}> +1</button>
+        <button class="btn btn-dark" on:click={removePoint}> -1</button>
+        <input type="number" bind:value={points}>
+        {/if}
+        
+    </div>
+</main>
+
+<style>
+	h1 {
+		color: #204f6e;
+	}
+	h3 {
+		margin-bottom: 30px;
+	}
+</style>
